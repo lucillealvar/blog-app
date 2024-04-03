e8d683d862850858e7e7703e976657eaff78f6f2
Lucille Alvar <lucille.alvar@gmail.com>
2024-04-03 16:56:31 -0600

    added pages with scss and routes for each page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './App.scss'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Projects from "./pages/Projects/Projects";
 
 const App = () => {
   return (
-    <>
-    APP
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
